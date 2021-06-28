21cca6a943ecc3834edf673e5d1fce57906efa2d
Mantra <Mantrasutaria007@mail.com>
2021-06-27 21:10:38 -0400

    Android Studio Commit

--- lib/welcomescreen.dart
+++ lib/welcomescreen.dart
@@ -11,102 +11,54 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          width: double.infinity,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("assets/images/homescreen.png"),
-              fit: BoxFit.cover,
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/homescreen.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: [
+            Container(
+              height: 60,
+              // color: Colors.white,
             ),
-          ),
-          child: Column(
-            children: [
-              Container(
-                height: 60,
-                // color: Colors.white,
+            // S Added code ---------------------
+
+            Container(
+              height: 500,
+              // color: Colors.white24,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(child: Text("#STOICHELP", style: rTextStyle)),
+                  SizedBox(
+                    width: 20,
+                    height: 20,
+                  ),
+
+                  Expanded(
+                      child: SizedBox(
+                        height: 34,
+                        width: 250,
+                        child: Container(
+                          child: Text(
+                        "Daily stoic inspiration to guide you through this ephemeral world.",
+                            textAlign: TextAlign.center,
+                    )),
+                      ),
+                  ),
+
+
+                ],
               ),
-              // S Added code ---------------------
+            )
 
-              Container(
-                height: 500,
-                // color: Colors.white24,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(child: Text("#STOICHELP", style: rTextStyle)),
-                    SizedBox(
-                      width: 20,
-                      height: 20,
-                    ),
-
-                    Expanded(
-                        child: SizedBox(
-                          height: 34,
-                          width: 250,
-                          child: Container(
-                            child: Text(
-                          "Daily stoic inspiration to guide you through this ephemeral world.",
-                              textAlign: TextAlign.center,
-                      )),
-                        ),
-                    ),
-
-                    // //Rotated text kit
-                    // Text(
-                    //   "Help with",
-                    //   style: lTextStyle,
-                    // ),
-                    // SizedBox(
-                    //   width: 200,
-                    //   height: 16,
-                    // ),
-                    // Container(
-                    //   child: DefaultTextStyle(
-                    //     style: rTextStyle,
-                    //     child: AnimatedTextKit(
-                    //       repeatForever: true,
-                    //       animatedTexts: [
-                    //         FadeAnimatedText(
-                    //           "ANXIETY",
-                    //           textStyle: rTextStyle,
-                    //         ),
-                    //         FadeAnimatedText(
-                    //           "DISCIPLINE",
-                    //           textStyle: rTextStyle,
-                    //         ),
-                    //         FadeAnimatedText(
-                    //           "REGRET",
-                    //           textStyle: rTextStyle,
-                    //         ),
-                    //         FadeAnimatedText(
-                    //           "CONFIDENCE",
-                    //           textStyle: rTextStyle,
-                    //         ),
-                    //         FadeAnimatedText(
-                    //           "STABILITY",
-                    //           textStyle: rTextStyle,
-                    //         ),
-                    //         FadeAnimatedText(
-                    //           "ACCEPTANCE",
-                    //           textStyle: rTextStyle,
-                    //         ),
-                    //         FadeAnimatedText(
-                    //           "ENVY",
-                    //           textStyle: rTextStyle,
-                    //         ),
-                    //       ],
-                    //     ),
-                    //   ),
-                    // ),
-                  ],
-                ),
-              )
-
-              // S Added code ---------------------
-            ],
-          ),
+            // S Added code ---------------------
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
