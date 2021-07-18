87757bb54c30fdc44096099a3c68ba8d4266d0f1
Mantra <Mantrasutaria007@mail.com>
2021-07-18 16:05:59 -0400

    Both are linkd

--- lib/welcomescreen.dart
+++ lib/welcomescreen.dart
@@ -3,7 +3,6 @@ import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'utilities/constants.dart';
 
